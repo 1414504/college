1d78ea320a1b1aaa787e6131d7791d5c41e0bf78
1414504 <1414504@rgu.ac.uk>
2016-08-18 01:28:23 +0100

    1

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
     
     
     <div class="row">
-        <div class="col-md-12"><h5><a href="catagory-college.php">All Students</a></h5></div>
+        <div class="col-md-12"><h5><a href="insert-college.php">All Students</a></h5></div>
     </div>
     
     
